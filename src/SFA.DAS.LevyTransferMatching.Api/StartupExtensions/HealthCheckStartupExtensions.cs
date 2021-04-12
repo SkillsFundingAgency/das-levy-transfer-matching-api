c7ac7f8cc87aebce039c939b503f176c853b44e7
chrisfoster76 <chrisfoster186@googlemail.com>
2021-04-12 11:01:56 +0100

    Adds boilerplate code

---
+++ src/SFA.DAS.LevyTransferMatching.Api/StartupExtensions/HealthCheckStartupExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using SFA.DAS.LevyTransferMatching.Api.HealthChecks;
+using SFA.DAS.LevyTransferMatching.Api.HttpResponseExtensions;
+
+namespace SFA.DAS.LevyTransferMatching.Api.StartupExtensions
+{
+    public static class HealthCheckStartupExtensions
+    {
+        public static IServiceCollection AddDasHealthChecks(this IServiceCollection services)
+        {
+            services
+                .AddHealthChecks()
+                .AddCheck<DummyHealthCheck>("Dummy health check");
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseDasHealthChecks(this IApplicationBuilder app)
+        {
+            app.UseHealthChecks("/ping", new HealthCheckOptions
+            {
+                Predicate = (_) => false,
+                ResponseWriter = (context, report) =>
+                {
+                    context.Response.ContentType = "application/json";
+                    return context.Response.WriteAsync("");
+                }
+            });
+
+            return app.UseHealthChecks("/health", new HealthCheckOptions
+            {
+                ResponseWriter = (c, r) => c.Response.WriteJsonAsync(new
+                {
+                    r.Status,
+                    r.TotalDuration,
+                    Results = r.Entries.ToDictionary(
+                        e => e.Key,
+                        e => new
+                        {
+                            e.Value.Status,
+                            e.Value.Duration,
+                            e.Value.Description,
+                            e.Value.Data
+                        })
+                })
+            });
+        }
+    }
+}
