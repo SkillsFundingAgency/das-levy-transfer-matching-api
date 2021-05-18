4611884ec5b657890d61c6c795d101085cf46636
dannygb <dan.beavon@education.gov.uk>
2021-05-18 17:11:46 +0100

    [TM-12] Fixes SQL healthcheck?

--- src/SFA.DAS.LevyTransferMatching.Api/StartupExtensions/HealthCheckStartupExtensions.cs
+++ src/SFA.DAS.LevyTransferMatching.Api/StartupExtensions/HealthCheckStartupExtensions.cs
@@ -2,9 +2,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using SFA.DAS.LevyTransferMatching.Api.HealthChecks;
 using SFA.DAS.LevyTransferMatching.Api.HttpResponseExtensions;
+using SFA.DAS.LevyTransferMatching.Data;
 using SFA.DAS.LevyTransferMatching.Infrastructure.Configuration;
 
 namespace SFA.DAS.LevyTransferMatching.Api.StartupExtensions
@@ -15,11 +18,10 @@ namespace SFA.DAS.LevyTransferMatching.Api.StartupExtensions
         {
             services
                 .AddHealthChecks()
-                .AddSqlServer(config.DatabaseConnectionString)
+                .AddDbContextCheck<LevyTransferMatchingDbContext>("Sql Health Check")
                 .AddCheck<NServiceBusHealthCheck>("NService Bus health check")
                 .AddRedis(config.RedisConnectionString, "Redis" )
             ;
-
 
             return services;
         }
