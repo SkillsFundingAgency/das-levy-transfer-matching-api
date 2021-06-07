5d6ee629dd159234706cdebfc15a2194f1e6684e
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-06-07 13:30:38 +0100

    Controller test

---
+++ src/SFA.DAS.LevyTransferMatching.Api.UnitTests/Controllers/PledgesControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using AutoFixture;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.LevyTransferMatching.Api.Controllers;
+using SFA.DAS.LevyTransferMatching.Api.Models;
+using SFA.DAS.LevyTransferMatching.Application.Commands.CreatePledge;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.LevyTransferMatching.Api.UnitTests.Controllers
+{
+    [TestFixture]
+    public class PledgesControllerTests
+    {
+        private Fixture _fixture;
+        private Mock<IMediator> _mockMediator;
+        private PledgesController _pledgesController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fixture = new Fixture();
+            _mockMediator = new Mock<IMediator>();
+            _pledgesController = new PledgesController(_mockMediator.Object);
+        }
+
+        [Test]
+        public async Task POST_Create_Returns_Accepted_With_Correct_Location()
+        {
+            // Arrange 
+            var encodedAccountId = _fixture.Create<string>();
+            var request = _fixture.Create<CreatePledgeRequest>();
+            var result = _fixture.Create<CreatePledgeResult>();
+
+            _mockMediator
+                .Setup(x => x.Send(It.IsAny<CreatePledgeCommand>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(result);
+
+            // Act
+            var actionResult = await _pledgesController.Create(encodedAccountId, request);
+            var acceptedResult = actionResult as AcceptedResult;
+
+            // Assert
+            Assert.IsNotNull(actionResult);
+            Assert.IsNotNull(acceptedResult);
+            Assert.AreEqual(acceptedResult.StatusCode, (int)HttpStatusCode.Accepted);
+            Assert.AreEqual(acceptedResult.Location, $"/accounts/{encodedAccountId}/pledges/{result.Id}");
+        }
+    }
+}
