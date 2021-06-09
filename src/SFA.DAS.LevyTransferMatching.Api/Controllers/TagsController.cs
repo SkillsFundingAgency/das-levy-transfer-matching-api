a9f96be471ee23d14803a2b1f9d3460c2311deea
chrisfoster76 <chrisfoster186@googlemail.com>
2021-06-09 15:39:59 +0100

    Implements level, jobrole, and sector as tags exposed from API

---
+++ src/SFA.DAS.LevyTransferMatching.Api/Controllers/TagsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.LevyTransferMatching.Extensions;
+using SFA.DAS.LevyTransferMatching.Models.Enums;
+
+namespace SFA.DAS.LevyTransferMatching.Api.Controllers
+{
+    [ApiController]
+    [Route("tags")]
+    public class TagsController : Controller
+    {
+        [Route("levels")]
+        [HttpGet]
+        public IActionResult Levels()
+        {
+            return Ok(EnumExtensions.ConvertToTags<Level>());
+        }
+
+        [Route("sectors")]
+        [HttpGet]
+        public IActionResult Sectors()
+        {
+            return Ok(EnumExtensions.ConvertToTags<Sector>());
+        }
+
+        [Route("jobRoles")]
+        [HttpGet]
+        public IActionResult JobRoles()
+        {
+            return Ok(EnumExtensions.ConvertToTags<JobRole>());
+        }
+    }
+}
--- src/SFA.DAS.LevyTransferMatching/Models/Enums/Level.cs
+++ src/SFA.DAS.LevyTransferMatching/Models/Enums/Level.cs
@@ -1,13 +1,20 @@
-﻿namespace SFA.DAS.LevyTransferMatching.Models.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SFA.DAS.LevyTransferMatching.Models.Enums
 {
     public enum Level
     {
-        None = 0,
+        [Display(Name = "Level 2 - GCSE")]
         Level2 = 1,
+        [Display(Name = "Level 3 - A level")]
         Level3 = 2,
+        [Display(Name = "Level 4 - higher national cerificate (HNC)")]
         Level4 = 3,
+        [Display(Name = "Level 5 - higher national diploma (HND)")]
         Level5 = 4,
+        [Display(Name = "Level 6 - degree")]
         Level6 = 5,
+        [Display(Name = "Level 7 - master’s degree")]
         Level7 = 6,
     }
 }
