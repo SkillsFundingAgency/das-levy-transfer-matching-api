a8e2916f0213aef0d3a84e75bb724e03814d604e
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-07-02 12:50:56 +0100

    Cleaning up

--- src/SFA.DAS.LevyTransferMatching/Application/Queries/GetPledge/GetPledgeResult.cs
+++ src/SFA.DAS.LevyTransferMatching/Application/Queries/GetPledge/GetPledgeResult.cs
@@ -5,9 +5,5 @@ namespace SFA.DAS.LevyTransferMatching.Application.Queries.GetPledge
 {
     public class GetPledgeResult : Pledge
     {
-        public object SingleOrDefault()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
