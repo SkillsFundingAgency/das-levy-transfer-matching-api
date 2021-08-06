ff6fc85307f43f367f728fe124d0c9b0b20fa530
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-08-06 10:29:18 +0100

    Pulling in DBContext reset code from CF's branch to fix 'tracking' test issues

--- src/SFA.DAS.LevyTransferMatching.UnitTests/DataFixture/LevyTransferMatchingDbContextFixture.cs
+++ src/SFA.DAS.LevyTransferMatching.UnitTests/DataFixture/LevyTransferMatchingDbContextFixture.cs
@@ -6,20 +6,25 @@ namespace SFA.DAS.LevyTransferMatching.UnitTests.DataFixture
 {
     public class LevyTransferMatchingDbContextFixture : IDisposable
     {
-        public LevyTransferMatchingDbContext DbContext { get; }
+        public LevyTransferMatchingDbContext DbContext { get; set; }
 
         public LevyTransferMatchingDbContextFixture()
+        {
+            ResetDbContext();
+        }
+
+        public void Dispose()
+        {
+            DbContext.Dispose();
+        }
+
+        protected void ResetDbContext()
         {
             var options = new DbContextOptionsBuilder<LevyTransferMatchingDbContext>()
                 .UseInMemoryDatabase($"SFA.DAS.LevyTransferMatching.Database_{DateTime.UtcNow.ToFileTimeUtc()}")
                 .Options;
 
             DbContext = new LevyTransferMatchingDbContext(options);
-        }
-
-        public void Dispose()
-        {
-            DbContext.Dispose();
         }
     }
 }
