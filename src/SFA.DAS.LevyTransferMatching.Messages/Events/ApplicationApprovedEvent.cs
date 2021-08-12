b9cef746ac18dda426c3c1227a66ccb3e4a291d5
chrisfoster76 <chrisfoster186@googlemail.com>
2021-08-12 11:00:45 +0100

    Adds approval message

---
+++ src/SFA.DAS.LevyTransferMatching.Messages/Events/ApplicationApprovedEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SFA.DAS.LevyTransferMatching.Messages.Events
+{
+    public class ApplicationApprovedEvent
+    {
+        public ApplicationApprovedEvent(int applicationId, DateTime approvedOn, int amount)
+        {
+            Amount = amount;
+            ApplicationId = applicationId;
+            ApprovedOn = approvedOn;
+        }
+
+        public int ApplicationId { get; }
+        public DateTime ApprovedOn { get; }
+        public int Amount { get; }
+    }
+}
