37b9f5bce7d52a44820d14c0db4a3e2fa4740a8f
chrisfoster76 <chrisfoster186@googlemail.com>
2021-08-12 12:21:39 +0100

    Adds event relay

--- src/SFA.DAS.LevyTransferMatching/Domain/EventHandlers/ApplicationApprovedHandler.cs
+++ src/SFA.DAS.LevyTransferMatching/Domain/EventHandlers/ApplicationApprovedHandler.cs
@@ -1,16 +1,26 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using SFA.DAS.LevyTransferMatching.Abstractions.Events;
 using SFA.DAS.LevyTransferMatching.Domain.Events;
+using SFA.DAS.LevyTransferMatching.Messages.Events;
+using SFA.DAS.NServiceBus.Services;
 
 namespace SFA.DAS.LevyTransferMatching.Domain.EventHandlers
 {
     public class ApplicationApprovedHandler : IDomainEventHandler<ApplicationApproved>
     {
-        public Task Handle(ApplicationApproved @event, CancellationToken cancellationToken = default(CancellationToken))
+        private readonly IEventPublisher _eventPublisher;
+
+        public ApplicationApprovedHandler(IEventPublisher eventPublisher)
         {
-            return Task.CompletedTask;
+            _eventPublisher = eventPublisher;
+        }
+
+        public async Task Handle(ApplicationApproved @event, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            await _eventPublisher.Publish(new ApplicationApprovedEvent(@event.ApplicationId,
+                @event.ApprovedOn,
+                @event.Amount));
         }
     }
 }
