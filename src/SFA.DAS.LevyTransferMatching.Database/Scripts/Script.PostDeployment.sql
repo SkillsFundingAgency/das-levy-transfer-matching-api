3690710130d463475087859a6c57f3bb380db235
Tony <abroodie+kraken@github.com>
2021-08-24 20:27:18 +0100

    added post deployment script

---
+++ src/SFA.DAS.LevyTransferMatching.Database/Scripts/Script.PostDeployment.sql
@@ -0,0 +1,21 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+/* Reporter Role Access */
+
+IF DATABASE_PRINCIPAL_ID('Reporter') IS NULL
+BEGIN
+    CREATE ROLE [Reporter]
+END
+
+GRANT SELECT ON Pledge TO Reporter
+GRANT SELECT ON Application TO Reporter
