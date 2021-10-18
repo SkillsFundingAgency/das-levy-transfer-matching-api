7eab2134d55523101cd2e395ecdb7e7a2632e60c
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-10-18 08:59:55 +0100

    Clearing up code smells ☢

--- src/SFA.DAS.LevyTransferMatching/Application/Commands/AcceptFunding/AcceptFundingCommandResult.cs
+++ src/SFA.DAS.LevyTransferMatching/Application/Commands/AcceptFunding/AcceptFundingCommandResult.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SFA.DAS.LevyTransferMatching.Application.Commands.AcceptFunding
+﻿namespace SFA.DAS.LevyTransferMatching.Application.Commands.AcceptFunding
 {
     public class AcceptFundingCommandResult
     {
