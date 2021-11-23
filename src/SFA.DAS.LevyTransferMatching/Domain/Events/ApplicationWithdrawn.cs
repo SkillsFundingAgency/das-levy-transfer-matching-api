ac74b6920f520f77ddb38ce6e4e1d9b4d3ee84a6
chrisfoster76 <chrisfoster186@googlemail.com>
2021-11-23 09:02:41 +0000

    Adds withdrawn and created domain events

---
+++ src/SFA.DAS.LevyTransferMatching/Domain/Events/ApplicationWithdrawn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SFA.DAS.LevyTransferMatching.Abstractions.Events;
+
+namespace SFA.DAS.LevyTransferMatching.Domain.Events
+{
+    public class ApplicationWithdrawn : IDomainEvent
+    {
+        public ApplicationWithdrawn(int applicationId, int pledgeId, DateTime withdrawnOn)
+        {
+            ApplicationId = applicationId;
+            WithdrawnOn = withdrawnOn;
+            PledgeId = pledgeId;
+        }
+
+        public int ApplicationId { get; }
+        public int PledgeId { get; }
+        public DateTime WithdrawnOn { get; }
+    }
+}
