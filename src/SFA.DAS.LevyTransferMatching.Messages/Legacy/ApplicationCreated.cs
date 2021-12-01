1bb606a6ff439d59122c62e844f7b37f2e4a3a18
chrisfoster76 <chrisfoster186@googlemail.com>
2021-12-01 10:31:40 +0000

    Adds legacy event

---
+++ src/SFA.DAS.LevyTransferMatching.Messages/Legacy/ApplicationCreated.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SFA.DAS.Messaging.Attributes;
+
+namespace SFA.DAS.LevyTransferMatching.Messages.Legacy
+{
+    [MessageGroup("pledge_application_created")]
+    public class PledgeApplicationCreated
+    {
+        public PledgeApplicationCreated()
+        {
+        }
+
+        public PledgeApplicationCreated(int applicationId, int pledgeId, DateTime createdOn, long transferSenderId)
+        {
+            ApplicationId = applicationId;
+            PledgeId = pledgeId;
+            CreatedOn = createdOn;
+            TransferSenderId = transferSenderId;
+        }
+
+        public int ApplicationId { get; }
+        public int PledgeId { get; }
+        public DateTime CreatedOn { get; }
+        public long TransferSenderId { get; set; }
+    }
+}
