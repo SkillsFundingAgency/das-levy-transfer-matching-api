599a2d6c28ee4ea36b77fb26647e586b2ea273e1
chrisfoster76 <chrisfoster186@googlemail.com>
2021-12-01 10:56:59 +0000

    Added attribute

---
+++ src/SFA.DAS.LevyTransferMatching.Messages/Legacy/MessageGroupAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SFA.DAS.LevyTransferMatching.Messages.Legacy
+{
+    public class MessageGroupAttribute : Attribute
+    {
+        public MessageGroupAttribute(string name = "")
+        {
+        }
+    }
+}
