23b1b166fe0906185eea75dd92d94932b4262717
chrisfoster76 <chrisfoster186@googlemail.com>
2021-12-09 15:03:51 +0000

    Switch to use id property

--- src/SFA.DAS.LevyTransferMatching/Domain/EventHandlers/ApplicationCreatedHandler.cs
+++ src/SFA.DAS.LevyTransferMatching/Domain/EventHandlers/ApplicationCreatedHandler.cs
@@ -23,7 +23,7 @@ namespace SFA.DAS.LevyTransferMatching.Domain.EventHandlers
         {
             var pledge = await _pledgeRepository.Get(@event.PledgeId);
 
-            var senderId = pledge.EmployerAccount.Id;
+            var senderId = pledge.EmployerAccountId;
 
             await _eventPublisher.Publish(new ApplicationCreatedEvent(@event.ApplicationId, @event.PledgeId,
                 @event.CreatedOn,
