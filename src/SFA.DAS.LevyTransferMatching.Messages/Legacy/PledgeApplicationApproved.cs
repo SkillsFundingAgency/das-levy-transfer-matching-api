dbe378ad85ca5c4db9fbd097084e7644e394dc94
chrisfoster76 <chrisfoster186@googlemail.com>
2021-12-16 08:44:19 +0000

    Adds further legacy message types

---
+++ src/SFA.DAS.LevyTransferMatching.Messages/Legacy/PledgeApplicationApproved.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SFA.DAS.LevyTransferMatching.Messages.Legacy
+{
+    [MessageGroup("pledge_application_approved")]
+    public class PledgeApplicationApproved
+    {
+        public PledgeApplicationApproved(int applicationId, int pledgeId, DateTime approvedOn, int amount, long transferSenderId)
+        {
+            ApplicationId = applicationId;
+            PledgeId = pledgeId;
+            Amount = amount;
+            ApprovedOn = approvedOn;
+            TransferSenderId = transferSenderId;
+        }
+
+        public int ApplicationId { get; }
+        public int PledgeId { get; }
+        public DateTime ApprovedOn { get; }
+        public int Amount { get; }
+        public long TransferSenderId { get; set; }
+    }
+}
---
+++ src/SFA.DAS.LevyTransferMatching.Messages/Legacy/PledgeApplicationWithdrawn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SFA.DAS.LevyTransferMatching.Messages.Legacy
+{
+    [MessageGroup("pledge_application_withdrawn")]
+    public class PledgeApplicationWithdrawn
+    {
+        public PledgeApplicationWithdrawn(int applicationId, int pledgeId, DateTime withdrawnOn, long transferSenderId)
+        {
+            ApplicationId = applicationId;
+            PledgeId = pledgeId;
+            WithdrawnOn = withdrawnOn;
+            TransferSenderId = transferSenderId;
+        }
+
+        public int ApplicationId { get; }
+        public int PledgeId { get; }
+        public DateTime WithdrawnOn { get; }
+        public long TransferSenderId { get; set; }
+    }
+}
