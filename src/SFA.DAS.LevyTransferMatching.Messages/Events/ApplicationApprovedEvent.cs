d52fe6ae474818079b29c0c62a9ead46ec860a24
Sajid Habib <s30x12@yahoo.com>
2022-01-14 15:41:27 +0000

    Initial check in to get nuget pkg out of it

--- src/SFA.DAS.LevyTransferMatching.Messages/Events/ApplicationApprovedEvent.cs
+++ src/SFA.DAS.LevyTransferMatching.Messages/Events/ApplicationApprovedEvent.cs
@@ -4,13 +4,14 @@ namespace SFA.DAS.LevyTransferMatching.Messages.Events
 {
     public class ApplicationApprovedEvent
     {
-        public ApplicationApprovedEvent(int applicationId, int pledgeId, DateTime approvedOn, int amount, long transferSenderId)
+        public ApplicationApprovedEvent(int applicationId, int pledgeId, DateTime approvedOn, int amount, long transferSenderId, long transferReceiverId)
         {
             ApplicationId = applicationId;
             PledgeId = pledgeId;
             Amount = amount;
             ApprovedOn = approvedOn;
             TransferSenderId = transferSenderId;
+            TransferReceiverId = transferReceiverId;
         }
 
         public int ApplicationId { get; }
@@ -18,5 +19,6 @@ namespace SFA.DAS.LevyTransferMatching.Messages.Events
         public DateTime ApprovedOn { get; }
         public int Amount { get; }
         public long TransferSenderId { get; set; }
+        public long TransferReceiverId { get; set; }
     }
 }
--- src/SFA.DAS.LevyTransferMatching/Domain/EventHandlers/ApplicationApprovedHandler.cs
+++ src/SFA.DAS.LevyTransferMatching/Domain/EventHandlers/ApplicationApprovedHandler.cs
@@ -12,23 +12,35 @@ namespace SFA.DAS.LevyTransferMatching.Domain.EventHandlers
     {
         private readonly IEventPublisher _eventPublisher;
         private readonly IPledgeRepository _pledgeRepository;
+        private readonly IApplicationRepository _applicationRepository;
 
-        public ApplicationApprovedHandler(IEventPublisher eventPublisher, IPledgeRepository pledgeRepository)
+        public ApplicationApprovedHandler(IEventPublisher eventPublisher, IPledgeRepository pledgeRepository, IApplicationRepository applicationRepository)
         {
             _eventPublisher = eventPublisher;
             _pledgeRepository = pledgeRepository;
+            _applicationRepository = applicationRepository;
         }
 
         public async Task Handle(ApplicationApproved @event, CancellationToken cancellationToken = default)
         {
-            var pledge = await _pledgeRepository.Get(@event.PledgeId);
+            var pledgeTask = _pledgeRepository.Get(@event.PledgeId);
 
-            var senderId = pledge.EmployerAccountId;
+            var applicationTask = _applicationRepository.Get(@event.ApplicationId);
+
+            await Task.WhenAll(pledgeTask, applicationTask);
+
+            var senderId = pledgeTask.Result.EmployerAccountId;
+
+            // TODO - Get other values as well for the email ...
+            // var emailAddresses = applicationTask.Result.EmailAddresses;
+
+            var receiverId = applicationTask.Result.EmployerAccount.Id;
 
             await _eventPublisher.Publish(new ApplicationApprovedEvent(@event.ApplicationId, @event.PledgeId,
                 @event.ApprovedOn,
                 @event.Amount,
-                senderId));
+                senderId,
+                receiverId));
         }
     }
 }
