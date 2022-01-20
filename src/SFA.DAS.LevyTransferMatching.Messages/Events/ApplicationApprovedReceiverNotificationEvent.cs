0f36f4430080e2d42f469930e8b382b7c06cc67e
Sajid Habib <s30x12@yahoo.com>
2022-01-20 14:50:38 +0000

    Added receiverAccId to the event

--- src/SFA.DAS.LevyTransferMatching.Messages/Events/ApplicationApprovedReceiverNotificationEvent.cs
+++ src/SFA.DAS.LevyTransferMatching.Messages/Events/ApplicationApprovedReceiverNotificationEvent.cs
@@ -4,6 +4,15 @@ namespace SFA.DAS.LevyTransferMatching.Messages.Events
 {
     public class ApplicationApprovedReceiverNotificationEvent
     {
+        public ApplicationApprovedReceiverNotificationEvent(int applicationId, int pledgeId, long transferSenderId, long transferReceiverId, long receiverAccountId)
+        {
+            ApplicationId = applicationId;
+            PledgeId = pledgeId;
+            TransferSenderId = transferSenderId;
+            TransferReceiverId = transferReceiverId;
+            ReceiverAccountId = receiverAccountId;
+        }
+
         public ApplicationApprovedReceiverNotificationEvent(int applicationId, int pledgeId, DateTime approvedOn, long transferSenderId, long transferReceiverId)
         {
             ApplicationId = applicationId;
@@ -13,10 +22,11 @@ namespace SFA.DAS.LevyTransferMatching.Messages.Events
             TransferReceiverId = transferReceiverId;
         }
 
+        public DateTime ApprovedOn { get; }
         public int ApplicationId { get; }
         public int PledgeId { get; }
-        public DateTime ApprovedOn { get; }
         public long TransferSenderId { get; set; }
         public long TransferReceiverId { get; set; }
+        public long ReceiverAccountId { get; set; }
     }
 }
