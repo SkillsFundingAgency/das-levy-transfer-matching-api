68d3129dc03cf464c468596c50ffbc7c0d7d9bce
chrisfoster76 <chrisfoster186@googlemail.com>
2022-01-26 08:58:52 +0000

    Adds backfill script

--- src/SFA.DAS.LevyTransferMatching.Database/Scripts/Script.PostDeployment.sql
+++ src/SFA.DAS.LevyTransferMatching.Database/Scripts/Script.PostDeployment.sql
@@ -19,3 +19,35 @@ END
 
 GRANT SELECT ON vwDashboardPledge TO DashbordReporting
 GRANT SELECT ON vwDashboardPledgeApplication TO DashbordReporting
+
+
+--TM-260 - Pledge Ledger backfill
+
+--Create opening balance entries
+insert into PledgeLedger (PledgeId, ApplicationId, UserAction, ActionDate, Amount, Balance)
+select Id, null, 'CreatePledge', CreatedOn, Amount, Amount
+from Pledge p
+
+--Create ApproveApplication entries (without ApplicationId)
+insert into PledgeLedger (PledgeId, ApplicationId, UserAction, ActionDate, Amount, Balance)
+select
+p.Id,
+null,
+'ApproveApplication', a.AuditDate,
+-(CONVERT(INT,JSON_VALUE(a.UpdatedState,'$.Amount')) - CONVERT(INT,JSON_VALUE(a.UpdatedState,'$.RemainingAmount'))),
+CONVERT(INT,JSON_VALUE(a.UpdatedState,'$.RemainingAmount'))
+from Audit a
+join Pledge p on p.Id = a.EntityId
+where a.UserAction = 'ApproveApplication' and a.EntityType = 'Pledge'
+
+--Create DeclineFunding entries (without ApplicationId)
+insert into PledgeLedger (PledgeId, ApplicationId, UserAction, ActionDate, Amount, Balance)
+select
+p.Id,
+null,
+'DeclineFunding', a.AuditDate,
+(CONVERT(INT,JSON_VALUE(a.InitialState,'$.Amount')) - CONVERT(INT,JSON_VALUE(a.InitialState,'$.RemainingAmount'))),
+CONVERT(INT,JSON_VALUE(a.UpdatedState,'$.RemainingAmount'))
+from Audit a
+join Pledge p on p.Id = a.EntityId
+where a.UserAction = 'DeclineFunding' and a.EntityType = 'Pledge'
