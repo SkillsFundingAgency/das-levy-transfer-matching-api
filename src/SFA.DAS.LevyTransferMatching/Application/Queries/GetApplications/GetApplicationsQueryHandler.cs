a5ef698837705703a8bf2509f521561463863f92
ogrwill <will.ogrady@digital.education.gov.uk>
2023-08-03 11:52:11 +0100

    MAP-295 fixing property name issue

--- src/SFA.DAS.LevyTransferMatching/Application/Queries/GetApplications/GetApplicationsQueryHandler.cs
+++ src/SFA.DAS.LevyTransferMatching/Application/Queries/GetApplications/GetApplicationsQueryHandler.cs
@@ -84,7 +84,7 @@ namespace SFA.DAS.LevyTransferMatching.Application.Queries.GetApplications
                     MatchLocation = x.MatchLocation,
                     CostingModel = x.CostingModel,
                     PledgeRemainingAmount = x.Pledge.RemainingAmount,
-                    AutoApproveFullMatches = x.Pledge.AutoApproveFullMatches
+                    PledgeAutomaticApprovalOption = x.Pledge.AutomaticApprovalOption
                 })
                 .AsNoTracking()
                 .AsSingleQuery()
