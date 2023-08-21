3b405b60509102db2e4f4531f62f27147e492172
Jon Harris <jon.harris@digital.education.gov.uk>
2023-08-21 13:05:16 +0100

    Added logging

--- src/SFA.DAS.LevyTransferMatching/Domain/EventHandlers/ApplicationCreatedHandler.cs
+++ src/SFA.DAS.LevyTransferMatching/Domain/EventHandlers/ApplicationCreatedHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.LevyTransferMatching.Abstractions.Events;
 using SFA.DAS.LevyTransferMatching.Data.Repositories;
 using SFA.DAS.LevyTransferMatching.Domain.Events;
@@ -12,15 +13,19 @@ namespace SFA.DAS.LevyTransferMatching.Domain.EventHandlers
     {
         private readonly IEventPublisher _eventPublisher;
         private readonly IPledgeRepository _pledgeRepository;
+        private readonly ILogger<ApplicationCreatedHandler> _logger;
 
-        public ApplicationCreatedHandler(IEventPublisher eventPublisher, IPledgeRepository pledgeRepository)
+        public ApplicationCreatedHandler(IEventPublisher eventPublisher, IPledgeRepository pledgeRepository, ILogger<ApplicationCreatedHandler> logger)
         {
             _eventPublisher = eventPublisher;
             _pledgeRepository = pledgeRepository;
+            _logger = logger;
         }
 
         public async Task Handle(ApplicationCreated @event, CancellationToken cancellationToken = default)
         {
+            _logger.LogInformation("{TypeName} started processing application with id: {Id}.", nameof(ApplicationCreatedHandler), @event.ApplicationId);
+            
             var pledge = await _pledgeRepository.Get(@event.PledgeId);
 
             var senderId = pledge.EmployerAccountId;
@@ -28,6 +33,8 @@ namespace SFA.DAS.LevyTransferMatching.Domain.EventHandlers
             await _eventPublisher.Publish(new ApplicationCreatedEvent(@event.ApplicationId, @event.PledgeId,
                 @event.CreatedOn,
                 senderId));
+            
+            _logger.LogInformation("{TypeName} completed processing.", nameof(ApplicationCreatedHandler));
         }
     }
 }
