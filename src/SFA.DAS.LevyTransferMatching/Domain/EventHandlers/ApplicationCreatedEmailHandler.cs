ac5babfb7ffd9c6a4565c0b06532311e1dffaf96
ogrwill <will.ogrady@digital.education.gov.uk>
2023-09-20 15:38:59 +0100

    MAP-333 adding new ApplicationCreatedEmail event

---
+++ src/SFA.DAS.LevyTransferMatching/Domain/EventHandlers/ApplicationCreatedEmailHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using SFA.DAS.LevyTransferMatching.Abstractions.Events;
+using SFA.DAS.LevyTransferMatching.Data.Repositories;
+using SFA.DAS.LevyTransferMatching.Domain.Events;
+using SFA.DAS.LevyTransferMatching.Messages.Events;
+using SFA.DAS.NServiceBus.Services;
+
+namespace SFA.DAS.LevyTransferMatching.Domain.EventHandlers
+{
+    public class ApplicationCreatedEmailHandler : IDomainEventHandler<ApplicationCreatedEmail>
+    {
+        private readonly IEventPublisher _eventPublisher;
+        private readonly IPledgeRepository _pledgeRepository;
+        private readonly IApplicationRepository _applicationRepository;
+
+        public ApplicationCreatedEmailHandler(IEventPublisher eventPublisher, IPledgeRepository pledgeRepository, IApplicationRepository applicationRepository)
+        {
+            _eventPublisher = eventPublisher;
+            _pledgeRepository = pledgeRepository;
+            _applicationRepository = applicationRepository;
+        }
+
+        public async Task Handle(ApplicationCreatedEmail @event, CancellationToken cancellationToken = default)
+        {
+            var pledge = await _pledgeRepository.Get(@event.PledgeId);
+            var application = await _applicationRepository.Get(@event.ApplicationId);
+
+            var senderId = pledge.EmployerAccountId;
+            var receiverId = application.EmployerAccount.Id;
+
+            await _eventPublisher.Publish(new ApplicationCreatedEmailEvent(@event.ApplicationId, @event.PledgeId, senderId, receiverId, @event.ReceiverAccountId));
+        }
+    }
+}
