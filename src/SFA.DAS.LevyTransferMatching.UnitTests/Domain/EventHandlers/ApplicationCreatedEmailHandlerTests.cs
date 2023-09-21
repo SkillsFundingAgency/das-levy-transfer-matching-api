705b3e743ee85d6b4447718e3721241f92551075
ogrwill <will.ogrady@digital.education.gov.uk>
2023-09-21 10:59:43 +0100

    MAP-333 simplifying email event object

--- src/SFA.DAS.LevyTransferMatching.UnitTests/Domain/EventHandlers/ApplicationCreatedEmailHandlerTests.cs
+++ src/SFA.DAS.LevyTransferMatching.UnitTests/Domain/EventHandlers/ApplicationCreatedEmailHandlerTests.cs
@@ -3,9 +3,6 @@ using System.Threading.Tasks;
 using AutoFixture;
 using Moq;
 using NUnit.Framework;
-using SFA.DAS.LevyTransferMatching.Data.Models;
-using SFA.DAS.LevyTransferMatching.Data.Repositories;
-using SFA.DAS.LevyTransferMatching.Data.ValueObjects;
 using SFA.DAS.LevyTransferMatching.Domain.EventHandlers;
 using SFA.DAS.LevyTransferMatching.Domain.Events;
 using SFA.DAS.LevyTransferMatching.Messages.Events;
@@ -20,37 +17,14 @@ namespace SFA.DAS.LevyTransferMatching.UnitTests.Domain.EventHandlers
         private ApplicationCreatedEmail _event;
         private Mock<IEventPublisher> _eventPublisher;
         private readonly Fixture _fixture = new Fixture();
-        private Mock<IPledgeRepository> _pledgeRepository;
-        private Mock<IApplicationRepository> _applicationRepository;
-        private long _transferSenderId;
-        private long _transferReceiverId;
 
         [SetUp]
         public void Setup()
         {
-            _event = _fixture.Create<ApplicationCreatedEmail>();
-
-            _transferSenderId = _fixture.Create<long>();
-            _transferReceiverId = _fixture.Create<long>();
-
+            _event = _fixture.Create<ApplicationCreatedEmail>();     
             _eventPublisher = new Mock<IEventPublisher>();
             _eventPublisher.Setup(x => x.Publish(It.IsAny<ApplicationCreatedEmailEvent>()));
-
-            var pledge = new Pledge(EmployerAccount.New(_transferSenderId, "Test Sender"), new CreatePledgeProperties(), UserInfo.System);
-
-            _pledgeRepository = new Mock<IPledgeRepository>();
-            _pledgeRepository.Setup(x => x.Get(It.Is<int>(p => p == _event.PledgeId)))
-                .ReturnsAsync(() => pledge);
-
-            var applicationProperties = new CreateApplicationProperties();
-
-            _applicationRepository = new Mock<IApplicationRepository>();
-            _applicationRepository.Setup(x => x.Get(It.Is<int>(p => p == _event.ApplicationId), 
-                                                    It.IsAny<int?>(), It.IsAny<long?>()))
-                .ReturnsAsync(() => new LevyTransferMatching.Data.Models.Application(It.IsAny<Pledge>(), EmployerAccount.New(_transferReceiverId, "Test Receiver"),
-                 applicationProperties, UserInfo.System));
-
-            _handler = new ApplicationCreatedEmailHandler(_eventPublisher.Object, _pledgeRepository.Object, _applicationRepository.Object);
+            _handler = new ApplicationCreatedEmailHandler(_eventPublisher.Object);
         }
 
         [Test]
@@ -61,8 +35,6 @@ namespace SFA.DAS.LevyTransferMatching.UnitTests.Domain.EventHandlers
             _eventPublisher.Verify(x => x.Publish(It.Is<ApplicationCreatedEmailEvent>(e =>
                     e.ApplicationId == _event.ApplicationId &&
                     e.PledgeId == _event.PledgeId &&
-                    e.TransferSenderId == _transferSenderId &&
-                    e.TransferReceiverId == _transferReceiverId &&
                     e.ReceiverAccountId == _event.ReceiverAccountId)));
         }
     }
--- src/SFA.DAS.LevyTransferMatching/Domain/EventHandlers/ApplicationCreatedEmailHandler.cs
+++ src/SFA.DAS.LevyTransferMatching/Domain/EventHandlers/ApplicationCreatedEmailHandler.cs
@@ -1,7 +1,6 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using SFA.DAS.LevyTransferMatching.Abstractions.Events;
-using SFA.DAS.LevyTransferMatching.Data.Repositories;
 using SFA.DAS.LevyTransferMatching.Domain.Events;
 using SFA.DAS.LevyTransferMatching.Messages.Events;
 using SFA.DAS.NServiceBus.Services;
@@ -11,25 +10,15 @@ namespace SFA.DAS.LevyTransferMatching.Domain.EventHandlers
     public class ApplicationCreatedEmailHandler : IDomainEventHandler<ApplicationCreatedEmail>
     {
         private readonly IEventPublisher _eventPublisher;
-        private readonly IPledgeRepository _pledgeRepository;
-        private readonly IApplicationRepository _applicationRepository;
 
-        public ApplicationCreatedEmailHandler(IEventPublisher eventPublisher, IPledgeRepository pledgeRepository, IApplicationRepository applicationRepository)
+        public ApplicationCreatedEmailHandler(IEventPublisher eventPublisher)
         {
-            _eventPublisher = eventPublisher;
-            _pledgeRepository = pledgeRepository;
-            _applicationRepository = applicationRepository;
+            _eventPublisher = eventPublisher;          
         }
 
         public async Task Handle(ApplicationCreatedEmail @event, CancellationToken cancellationToken = default)
-        {
-            var pledge = await _pledgeRepository.Get(@event.PledgeId);
-            var application = await _applicationRepository.Get(@event.ApplicationId);
-
-            var senderId = pledge.EmployerAccountId;
-            var receiverId = application.EmployerAccount.Id;
-
-            await _eventPublisher.Publish(new ApplicationCreatedEmailEvent(@event.ApplicationId, @event.PledgeId, senderId, receiverId, @event.ReceiverAccountId));
+        {          
+            await _eventPublisher.Publish(new ApplicationCreatedEmailEvent(@event.ApplicationId, @event.PledgeId, @event.ReceiverAccountId));
         }
     }
 }
