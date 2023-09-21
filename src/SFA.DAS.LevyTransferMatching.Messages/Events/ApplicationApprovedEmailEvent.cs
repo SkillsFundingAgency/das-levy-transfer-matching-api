07ce15c8afb6d904d647184e96707319bf6ebf65
ogrwill <will.ogrady@digital.education.gov.uk>
2023-09-21 16:12:55 +0100

    MAP-333 email for successful application

--- src/SFA.DAS.LevyTransferMatching.Messages/Events/ApplicationApprovedEmailEvent.cs
+++ src/SFA.DAS.LevyTransferMatching.Messages/Events/ApplicationApprovedEmailEvent.cs
@@ -4,19 +4,15 @@ namespace SFA.DAS.LevyTransferMatching.Messages.Events
 {
     public class ApplicationApprovedEmailEvent
     {
-        public ApplicationApprovedEmailEvent(int applicationId, int pledgeId, long transferSenderId, long transferReceiverId, long receiverAccountId)
+        public ApplicationApprovedEmailEvent(int applicationId, int pledgeId, long receiverAccountId)
         {
             ApplicationId = applicationId;
             PledgeId = pledgeId;
-            TransferSenderId = transferSenderId;
-            TransferReceiverId = transferReceiverId;
             ReceiverAccountId = receiverAccountId;
         }
 
         public int ApplicationId { get; }
-        public int PledgeId { get; }
-        public long TransferSenderId { get; set; }
-        public long TransferReceiverId { get; set; }
+        public int PledgeId { get; }      
         public long ReceiverAccountId { get; set; }
     }
 }
