b7e9c726eac3654de489959cfe43d2a165fb3ad3
ogrwill <will.ogrady@digital.education.gov.uk>
2023-09-29 14:35:36 +0100

    MAP-333  updating application approved and rejected events to include receiverAccountId needed for emails.

--- src/SFA.DAS.LevyTransferMatching/Domain/Events/ApplicationRejected.cs
+++ src/SFA.DAS.LevyTransferMatching/Domain/Events/ApplicationRejected.cs
@@ -5,17 +5,19 @@ namespace SFA.DAS.LevyTransferMatching.Domain.Events
 {
     public class ApplicationRejected : IDomainEvent
     {
-        public ApplicationRejected(int applicationId, int pledgeId, DateTime rejectedOn, int amount)
+        public ApplicationRejected(int applicationId, int pledgeId, DateTime rejectedOn, int amount, long receiverAccountId)
         {
             Amount = amount;
             ApplicationId = applicationId;
             RejectedOn = rejectedOn;
             PledgeId = pledgeId;
+            ReceiverAccountId = receiverAccountId;
         }
 
         public int ApplicationId { get; }
         public int PledgeId { get; }
         public DateTime RejectedOn { get; }
         public int Amount { get; }
+        public long ReceiverAccountId { get; set; }
     }
 }
