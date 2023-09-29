2750011cec4b27a970a8a94e4692b75dade28f98
ogrwill <will.ogrady@digital.education.gov.uk>
2023-09-29 13:45:48 +0100

    MAP-333 removing email events. Updateing ApplicationCreatedEvent

--- src/SFA.DAS.LevyTransferMatching.Messages/Events/ApplicationCreatedEvent.cs
+++ src/SFA.DAS.LevyTransferMatching.Messages/Events/ApplicationCreatedEvent.cs
@@ -4,17 +4,20 @@ namespace SFA.DAS.LevyTransferMatching.Messages.Events
 {
     public class ApplicationCreatedEvent
     {
-        public ApplicationCreatedEvent(int applicationId, int pledgeId, DateTime createdOn, long transferSenderId)
+        public ApplicationCreatedEvent(int applicationId, int pledgeId, DateTime createdOn, long transferSenderId, long receiverAccountId)
         {
             ApplicationId = applicationId;
             PledgeId = pledgeId;
             CreatedOn = createdOn;
             TransferSenderId = transferSenderId;
+            ReceiverAccountId = receiverAccountId;
         }
 
         public int ApplicationId { get; }
         public int PledgeId { get; }
         public DateTime CreatedOn { get; }
         public long TransferSenderId { get; set; }
+        public long ReceiverAccountId { get; set; }
+
     }
 }
--- src/SFA.DAS.LevyTransferMatching/Domain/Events/ApplicationCreated.cs
+++ src/SFA.DAS.LevyTransferMatching/Domain/Events/ApplicationCreated.cs
@@ -5,15 +5,18 @@ namespace SFA.DAS.LevyTransferMatching.Domain.Events
 {
     public class ApplicationCreated : IDomainEvent
     {
-        public ApplicationCreated(int applicationId, int pledgeId, DateTime createdOn)
+        public ApplicationCreated(int applicationId, int pledgeId, long receiverAccountId, DateTime createdOn)
         {
             ApplicationId = applicationId;
             CreatedOn = createdOn;
             PledgeId = pledgeId;
+            ReceiverAccountId = receiverAccountId;
         }
 
         public int ApplicationId { get; }
         public int PledgeId { get; }
+        public long ReceiverAccountId { get; set; }
+
         public DateTime CreatedOn { get; }
     }
 }
