2e2c78fe7ef1292a222c3172f695da1c04f01a15
ogrwill <will.ogrady@digital.education.gov.uk>
2024-04-24 15:41:02 +0100

    adding post-deploy script to close pledges under 2000

--- src/SFA.DAS.LevyTransferMatching.Database/Scripts/Script.PostDeployment.sql
+++ src/SFA.DAS.LevyTransferMatching.Database/Scripts/Script.PostDeployment.sql
@@ -23,4 +23,6 @@ GRANT SELECT ON vwDashboardPledgeApplication TO DashbordReporting
 
 --CON-5105: Update existing applications to switch to new costing model
 --This script can be safely re-executed but can be removed at any time following deployment
-update Application set [CostingModel] = 1 where [Status] = 0 and [CostingModel] = 0
+update Application set [CostingModel] = 1 where [Status] = 0 and [CostingModel] = 0;
+
+:r .\ClosePledgesUnder2000.sql
